e1774e8d2fd022f14615ae3d92004bb8ae461b90
e_ntyo <entyo.contact@gmail.com>
2019-07-28 01:51:04 +0900

    SPA

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// import "firebase/auth";
+import React from "react";
+import { render } from "react-dom";
+import "./index.scss";
+
+function App() {
+  return <h1>Hello</h1>;
+}
+
+render(<App />, document.getElementById("root"));
---
+++ webpack.config.ts
@@ -0,0 +1,85 @@
+import { join, resolve } from "path";
+
+import dotenv from "dotenv";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack, { DefinePlugin } from "webpack";
+
+module.exports = (_, argv: webpack.Configuration) => {
+  // production -> '.env'
+  // development/none -> '.env.dev'
+  const path = join(
+    __dirname,
+    argv.mode === "production" ? ".env" : ".env.dev"
+  );
+
+  const fileEnv = dotenv.config({ path }).parsed;
+
+  const envKeys: { [K in string]: string } = Object.keys(fileEnv).reduce(
+    (prev, next) => {
+      prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+      return prev;
+    },
+    {}
+  );
+
+  return {
+    entry: "./src/index.tsx",
+    devtool: "inline-source-map",
+    output: {
+      path: join(__dirname, "public"),
+      filename: "bundle.js",
+      // 'Cannot GET /hogehoge' 対策
+      publicPath: "/"
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.tsx?$/,
+          use: "ts-loader"
+        },
+        {
+          exclude: /node_modules/,
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "style-loader" // Creates style nodes from JS strings
+            },
+            {
+              loader: "css-loader" // Translates CSS into CommonJS
+            },
+            {
+              loader: "sass-loader" // Compiles Sass to CSS
+            }
+          ]
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: "file-loader"
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          loader: "file-loader"
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html"
+      }),
+      new DefinePlugin(envKeys)
+    ],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    devServer: {
+      inline: true,
+      contentBase: resolve(__dirname, "dist"),
+      historyApiFallback: true,
+      watchContentBase: true,
+      hot: true,
+      open: true,
+      port: 8888
+    }
+  };
+};
